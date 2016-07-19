baf84e6fdd6b35eacae22ace56e13c5f760194fc
nikvaessen <nikvaes@gmail.com>
2016-07-19 10:59:22 +0200

    new test

--- src/test/java/TranscribingTest.java
+++ src/test/java/TranscribingTest.java
@@ -9,7 +9,7 @@ public class TranscribingTest
 {
     public static void main(String[] args) throws Exception
     {
-        File file = new File("src/test/resources/test.webm");
+        File file = new File("src/test/resources/caffeine.wav");
         AudioTranscriber transcriber = new AudioTranscriber();
         System.out.println("result: " + transcriber.transcribeAudioFile(file));
     }
