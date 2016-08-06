690e611a7cec580a7fcb99ecdd00323a3535f3a6
nikvaessen <nikvaes@gmail.com>
2016-08-06 13:23:19 +0200

    starting server without arguments now uses the port specified in the properties file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
   then
-    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" -Dexec.args="8081"
+    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer"
   else
     mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" -Dexec.args="$1"
 fi
